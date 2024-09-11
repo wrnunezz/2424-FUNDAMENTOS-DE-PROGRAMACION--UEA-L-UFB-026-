f28e0580a4674e29473aa9e6265e902f6b76d87b
wrnunezz <wr.nunezz@uea.edu.ec>
2024-09-11 18:28:49 -0500

    clase de Tutoría del 11/09/2024

--- 3.2 Operaciones con Arreglos/ejercicio 2.py
+++ 3.2 Operaciones con Arreglos/ejercicio 2.py
@@ -1,7 +1,25 @@
 #ejercicio 2 Matriz de 3 x 3
 matriz = [
-    [8, 13, 9],
-    [2, 10, 4],
-    [6, 8, 3]
+    [1, 13, 9],
+    [2, 11, 4],
+    [3, 8, 3]
 ]
+
+print(matriz)
+# metodo de ordenamiento buble_sort
+def buble_sort(fila):
+    #algoritmo buble sort
+    n = len(fila)
+    for i in range(n):
+        for j in range(n-1, i,-1):
+            if fila[j] > fila[j-1]:
+                fila[j], fila[j-1] = fila[j-1], fila[j]
+                print(fila)
+
+print("Matriz original ")
+print(matriz)
+buble_sort(matriz[0])
+print(matriz)
+
+
 
